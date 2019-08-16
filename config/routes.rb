4bf90fb4b7b2839c82b96b89fdb490cc4c457ec3
赤穂浩司 <akosiako@akahokoujinoMacBook-Pro.local>
2019-08-16 15:51:43 +0700

    aaaaaaaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  get 'users/new'
+
   root "static_pages#home"
   
   get '/help', to: 'static_pages#help'
@@ -6,5 +9,6 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
 
   get '/contact', to: 'static_pages#contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/signup', to: 'users#new'
 end
