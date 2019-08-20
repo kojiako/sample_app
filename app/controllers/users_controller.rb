f35b73d48a7df731fe44699b86ec7d6567b95ed9
赤穂浩司 <akosiako@akahokoujinoMacBook-Pro.local>
2019-08-20 15:26:30 +0700

    AAAAAAaaaaaaaaaaaaaa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+     @user = User.find(params[:id])
+  end
+
+  def update
+
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報更新成功！"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+  
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
 end
