e5e70daf540bbff9e63199932bfe080744d66cf5
赤穂浩司 <akosiako@akahokoujinoMacBook-Pro.local>
2019-08-21 10:48:06 +0700

    AAAAAABBBBBBBBBBBBBBBBBBBBB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page:params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -19,7 +26,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-     @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
@@ -32,11 +39,24 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください"
+      redirect_to root_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    unless current_user?(@user)
+      redirect_to root_url
+    end
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,4 +18,7 @@ module SessionsHelper
     @current_user = nil
   end
 
+  def current_user?(user)
+    user == current_user
+  end
 end
